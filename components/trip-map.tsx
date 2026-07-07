e59633924484a14932852e27414a051232de4716
JackVR666 <107625913+JackVR666@users.noreply.github.com>
2026-07-07 12:01:23 +0200

    Sostituita mappa dark con versione chiara

--- components/trip-map.tsx
+++ components/trip-map.tsx
@@ -51,7 +51,7 @@ export default function TripMap({ points }: { points: TrackPoint[] }) {
     >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+        url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
       />
 
       {track.length > 1 && (
