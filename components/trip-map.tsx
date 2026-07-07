f8501a42a1ea1890c29a515a1493261f632eb39d
JackVR666 <107625913+JackVR666@users.noreply.github.com>
2026-07-07 12:15:21 +0200

    Sostituita mappa openstreeet

--- components/trip-map.tsx
+++ components/trip-map.tsx
@@ -51,7 +51,7 @@ export default function TripMap({ points }: { points: TrackPoint[] }) {
     >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
 
       {track.length > 1 && (
