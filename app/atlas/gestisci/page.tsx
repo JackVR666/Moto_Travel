a9dd849ab68f4f343b2a69bd6fff4307a74796d8
JackVR666 <107625913+JackVR666@users.noreply.github.com>
2026-07-29 09:25:12 +0200

    v. 4.1

--- app/atlas/gestisci/page.tsx
+++ app/atlas/gestisci/page.tsx
@@ -477,7 +477,7 @@ export default function GestisciLuoghiPage() {
                     <option value="spiaggia">Spiaggia</option>
                     <option value="ristorante">Ristorante</option>
                     <option value="hotel">Hotel</option>
-                    <option value="hotel">Lavoro</option>
+                    <option value="Lavoro">Lavoro</option>
                     <option value="altro">Altro</option>
                   </select>
                 </div>
